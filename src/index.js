ce7bbc47aef15d805c60f92761842320a8d75eb3
Vicentejavier23 <vi.bueno@duocuc.cl>
2026-04-22 20:25:54 -0400

    solucion de url

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors({
+  origin: '*'
+}));
+
 const PORT = process.env.PORT || 3000;
 const coursesRouter = require("./routes/courses");
 const authRouter = require("./routes/auth")
 
-app.use(cors({
-  origin: 'https://learnmore-frontend-phi.vercel.app', // Tu URL de Vercel sin la barra final
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+
 app.use(express.json());
+
 app.use('/courses', coursesRouter);
 app.use('/auth',authRouter)
 
